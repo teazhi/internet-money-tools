a9c21c889a79295f3249aaf3bd6fef8b5229a4d3
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2025-08-18 14:38:05 -0400

    Update Dashboard.js

--- dashboard/frontend/src/components/Dashboard.js
+++ dashboard/frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../contexts/AuthContext';
